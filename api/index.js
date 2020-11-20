e6a51d016754c4df95ffd304b9dcc164d89795e8
Gaston Ferreyra <60433890+toninence@users.noreply.github.com>
2020-11-20 12:57:47 -0300

    deploy

--- api/index.js
+++ api/index.js
@@ -3,5 +3,5 @@ const server = require('./src/App.js');
 var port = process.env.PORT || 3000
 
   server.listen(port, () => {
-    console.log('%s listening at ' + port); // eslint-disable-line no-console
+    console.log('%s listening at port: ' + port); // eslint-disable-line no-console
   });
