ab74356a7e253b6f18bd7fe6f9dceca2037e801e
Gaston Ferreyra <60433890+toninence@users.noreply.github.com>
2020-11-20 12:39:40 -0300

    deploy

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const server = require('./src/App.js');
 
-var port = 3000
+var port = process.env.PORT || 3000
 
   server.listen(port, () => {
     console.log('%s listening at ' + port); // eslint-disable-line no-console
--- api/src/App.js
+++ api/src/App.js
@@ -1,20 +1,27 @@
 const { default: Axios } = require('axios');
 const express = require ('express');
 const axios = require ('axios');
+var path = require("path")
 
 const app = express();
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
-    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Methods','GET, PUT, POST, DELETE')
-    res.header('Access-Control-Allow-Headers', '*, X-Requested-With, Content-Type, Accept');
-    next();
-  });
+app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/:username', (req, res)=>{
+
+
+
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', 'http://localhost:3001'); // update to match the domain you will make the request from
+//     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+//     res.header('Access-Control-Allow-Credentials', 'true');
+//     res.header('Access-Control-Allow-Methods','GET, PUT, POST, DELETE')
+//     res.header('Access-Control-Allow-Headers', '*, X-Requested-With, Content-Type, Accept');
+//     next();
+//   });
+
+app.get('/api/:username', (req, res)=>{
     const { username } = req.params
+    console.log(username);
     axios.get(`https://bio.torre.co/api/bios/${username}`)
     .then(response => {
         console.log(response)
@@ -22,6 +29,8 @@ app.get('/:username', (req, res)=>{
     })
     
 });
-
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
 
 module.exports = app;
